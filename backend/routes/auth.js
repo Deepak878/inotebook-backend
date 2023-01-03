c0ca1e92830f1bc6f508d9873e457ef4fd8c6824
Deepak878 <54785214+Deepak878@users.noreply.github.com>
2023-01-03 17:09:51 +0545

    third commit final

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "Harryisagood$oy";
+//Create a User using: POST "/api/auth/createuser"Doesn't require auth
 
-router.get("/", (req, res) => {
-  res.send("Hllo");
-});
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //if there are errors, return bad request and error message
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //check whether the user with same email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const securePassword = await bcrypt.hash(req.body.password, salt);
+      //create a new user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: securePassword,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      console.log(authtoken);
+      res.json({ authtoken });
+      //catch errors
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("SOme Error Occured");
+    }
+  }
+);
 
 module.exports = router;
